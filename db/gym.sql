ff186898091409b5f99fd0d3ade4a22755d5e75a
FKeir <fmkeir@gmail.com>
2020-02-08 15:38:12 +0000

    Initial commit

---
+++ db/gym.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS sessions;
+
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  goal VARCHAR(255)
+);
+
+CREATE TABLE sessions (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(255),
+  trainer VARCHAR(255),
+  Capacity INT
+);
+
+CREATE TABLE bookings (
+  id SERIAL PRIMARY KEY,
+  member_id INT REFERENCES members(id),
+  session_id INT REFERENCES sessions(id)
+);
