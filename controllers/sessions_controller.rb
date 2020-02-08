17c07c5328eb910be035cff8dad4c57f36ed4cc8
FKeir <fmkeir@gmail.com>
2020-02-08 17:55:52 +0000

    Add edit/update routes to sessions

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -14,7 +14,17 @@ get '/sessions/:id' do
   erb(:"sessions/show")
 end
 
+get '/sessions/:id/edit' do
+  @session = Session.find(params['id'])
+  erb(:"sessions/edit")
+end
+
 post '/sessions' do
   Session.new(params).save()
+  redirect to '/sessions'
+end
+
+post '/sessions/:id' do
+  Session.new(params).update()
   redirect to '/sessions'
 end
