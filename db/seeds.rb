378cb1ecd80b46ce86922fdf451137a735afd0a1
FKeir <fmkeir@gmail.com>
2020-02-08 16:17:50 +0000

    Add session model with full CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,35 @@
 require('pry')
 require_relative('../models/member')
+require_relative('../models/session')
 
 Member.delete_all()
+Session.delete_all()
 
 member1 = Member.new({
   "first_name" => "Graham",
   "last_name" => "Grahamson",
   "goal" => "Get stronger"
   })
-member2 = Member.new({"first_name" => "Nil",
+member2 = Member.new({
+  "first_name" => "Nil",
   "last_name" => "Nilson",
   "goal" => "Get faster"
   })
 member1.save()
 member2.save()
+
+session1 = Session.new({
+  "type" => "Graham",
+  "trainer" => "George Smith",
+  "capacity" => 5
+  })
+session2 = Session.new({
+  "type" => "Nil",
+  "trainer" => "Nicole Greene",
+  "capacity" => 30
+  })
+session1.save()
+session2.save()
 
 binding.pry
 nil
---
+++ models/session.rb
@@ -0,0 +1,54 @@
+require_relative('../db/sql_runner')
+
+class Session
+  attr_accessor :type, :trainer, :capacity
+  attr_reader :id
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @type = options["type"]
+    @trainer = options["trainer"]
+    @capacity = options["capacity"].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO sessions
+    (type, trainer, capacity)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id"
+    values = [@type, @trainer, @capacity]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i
+  end
+
+  def update()
+    sql = "UPDATE sessions
+    SET (type, trainer, capacity)
+    = ($1, $2, $3)
+    WHERE id = $4"
+    values = [@type, @trainer, @capacity, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM sessions WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM sessions"
+    return SqlRunner.run(sql).map {|session| Session.new(session)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM sessions"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM sessions WHERE id = $1"
+    values = [id]
+    return Session.new(SqlRunner.run(sql, values)[0])
+  end
+end
