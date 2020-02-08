9d736829a217e4995611154ba740235cfc44a0de
FKeir <fmkeir@gmail.com>
2020-02-08 18:19:08 +0000

    Add delete route to bookings

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -9,3 +9,8 @@ get "/bookings/:id" do
   @booking = Booking.find(params["id"])
   erb(:"bookings/show")
 end
+
+post "/bookings/:id/delete" do
+  Booking.find(params[:id]).delete()
+  redirect to "/bookings"
+end
