8a646062567b7e64cff48ec8a22058f79247e1f2
FKeir <fmkeir@gmail.com>
2020-02-08 18:23:40 +0000

    Add new/create routes to bookings

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -5,9 +5,18 @@ get "/bookings" do
   erb(:"bookings/index")
 end
 
+get '/bookings/new' do
+  erb(:"bookings/new")
+end
+
 get "/bookings/:id" do
   @booking = Booking.find(params["id"])
   erb(:"bookings/show")
+end
+
+post '/bookings' do
+  Booking.new(params).save()
+  redirect to '/bookings'
 end
 
 post "/bookings/:id/delete" do
