0ddad9fc6e3a2746527917a740d7b334713c06c0
FKeir <fmkeir@gmail.com>
2020-02-08 17:37:03 +0000

    Add edit/create views to members

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -5,6 +5,10 @@ get '/members' do
   erb(:'members/index')
 end
 
+get '/members/new' do
+  erb(:"members/new")
+end
+
 get '/members/:id' do
   @member = Member.find(params['id'])
   erb(:"members/show")
@@ -13,6 +17,11 @@ end
 get '/members/:id/edit' do
   @member = Member.find(params['id'])
   erb(:"members/edit")
+end
+
+post '/members' do
+  Member.new(params).save()
+  redirect to '/members'
 end
 
 post '/members/:id' do
