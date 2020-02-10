0f1f7b6cef95226ae29d3ebd4519680b5afb5b34
FKeir <fmkeir@gmail.com>
2020-02-10 15:08:32 +0000

    Add CRUD to room model

--- models/room.rb
+++ models/room.rb
@@ -9,4 +9,39 @@ class Room
     @name = options["name"]
     @capacity = options["capacity"]
   end
+
+  def save()
+    sql = "INSERT INTO rooms
+    (name, capacity)
+    VALUES
+    ($1, $2)
+    RETURNING id"
+    values = [@name, @capacity]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i
+  end
+
+  def update()
+    sql = "UPDATE rooms
+    SET (name, capacity)
+    = ($1, $2)
+    WHERE id = $3"
+    values = [@name, @capacity, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM rooms WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM rooms"
+    return SqlRunner.run(sql).map {|room| Room.new(room)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM rooms"
+    SqlRunner.run(sql)
+  end
 end
