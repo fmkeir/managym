da2678dbc6ff956e1fdb0cf33378180ee8a28adc
FKeir <fmkeir@gmail.com>
2020-02-11 13:17:24 +0000

    Add more entries to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,13 +31,55 @@ member1 = Member.new({
   "goal" => "Get stronger"
   })
 member2 = Member.new({
-  "membership_id" => off_peak.id,
+  "membership_id" => standard.id,
   "first_name" => "Nil",
   "last_name" => "Nilson",
   "goal" => "Get faster"
   })
+member3 = Member.new({
+  "membership_id" => standard.id,
+  "first_name" => "Sara",
+  "last_name" => "Sigmundsdottir",
+  "goal" => "Get faster"
+  })
+member4 = Member.new({
+  "membership_id" => standard.id,
+  "first_name" => "Kara",
+  "last_name" => "Webb",
+  "goal" => "Rebuild fitness"
+  })
+member5 = Member.new({
+  "membership_id" => standard.id,
+  "first_name" => "Mat",
+  "last_name" => "Fraser",
+  "goal" => "Eliminate weaknesses"
+  })
+member6 = Member.new({
+  "membership_id" => standard.id,
+  "first_name" => "Frederik",
+  "last_name" => "Aegidius",
+  "goal" => "Maintain fitness"
+  })
+member7 = Member.new({
+  "membership_id" => off_peak.id,
+  "first_name" => "Jamie",
+  "last_name" => "Greene",
+  "goal" => "Have fun"
+  })
+member8 = Member.new({
+  "membership_id" => off_peak.id,
+  "first_name" => "John",
+  "last_name" => "Jacob",
+  "goal" => "Get hench"
+  })
 member1.save()
 member2.save()
+member3.save()
+member4.save()
+member5.save()
+member6.save()
+member7.save()
+member8.save()
 
 room1 = Room.new({
   "name" => "Studio",
@@ -55,7 +97,7 @@ session1 = Session.new({
   "trainer" => "George Smith",
   "room_id" => room1.id,
   "start_time" => "2020-02-14 15:30:00",
-  "duration" => 60
+  "duration" => 30
   })
 session2 = Session.new({
   "type" => "Powerlifting",
@@ -69,14 +111,44 @@ session2.save()
 
 booking1 = Booking.new({
   "member_id" => member1.id,
-  "session_id" => session2.id
+  "session_id" => session1.id
   })
 booking2 = Booking.new({
   "member_id" => member2.id,
+  "session_id" => session1.id
+  })
+booking3 = Booking.new({
+  "member_id" => member3.id,
+  "session_id" => session1.id
+  })
+booking4 = Booking.new({
+  "member_id" => member4.id,
+  "session_id" => session1.id
+  })
+booking5 = Booking.new({
+  "member_id" => member5.id,
+  "session_id" => session2.id
+  })
+booking6 = Booking.new({
+  "member_id" => member6.id,
+  "session_id" => session2.id
+  })
+booking7 = Booking.new({
+  "member_id" => member7.id,
+  "session_id" => session2.id
+  })
+booking8 = Booking.new({
+  "member_id" => member8.id,
   "session_id" => session2.id
   })
 booking1.save()
 booking2.save()
+booking3.save()
+booking4.save()
+booking5.save()
+booking6.save()
+booking7.save()
+booking8.save()
 
 binding.pry
 nil
