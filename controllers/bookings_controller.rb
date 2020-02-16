08e8124ad92cb04353c5a69f8c9b245d54d74923
FKeir <fmkeir@gmail.com>
2020-02-16 21:44:44 +0000

    On booking redirect to member who booked into the session

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -12,22 +12,23 @@ get '/bookings/new' do
   @sessions = Session.all()
   erb(:"bookings/new")
 end
-#
+
 # get "/bookings/:id" do
 #   @booking = Booking.find(params["id"])
 #   erb(:"bookings/show")
 # end
-#
+
 post '/bookings' do
   member = Member.find(params["member_id"])
   session = Session.find(params["session_id"])
   @message = Booking.create(member, session)
-  @message == "Done" ? (redirect to '/bookings') : erb(:"bookings/error_create")
+  member_page = "/members/#{params["member_id"]}"
+  @message == "Done" ? (redirect to member_page) : erb(:"bookings/error_create")
 end
-#
-# post "/bookings/:id/delete" do
-#   Booking.find(params[:id]).delete()
-#   redirect to "/bookings"
-# end
+
+post "/bookings/:id/delete" do
+  Booking.find(params[:id]).delete()
+  redirect to "/bookings"
+end
 
 # Bookings can't be edited only created/deleted
