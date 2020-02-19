6d695424af24bfc5684ad07e618cc593d5ceb6ba
FKeir <fmkeir@gmail.com>
2020-02-19 12:15:23 +0000

    Change random_booking to loop through members one at a time

--- helpers/seeding.rb
+++ helpers/seeding.rb
@@ -1,8 +1,14 @@
-def random_booking(members, sessions, number_of_bookings)
-  bookings_made = 0
-  until bookings_made == number_of_bookings
-    if Booking.create(members.sample(), sessions.sample()) == "Done"
-      bookings_made += 1
+def random_booking(members, sessions, max_member_bookings)
+  number_of_sessions = sessions.count()
+  for member in members
+    member_bookings = rand((number_of_sessions/3)..max_member_bookings)
+    bookings_made = 0
+    attempts = 0
+    until bookings_made == member_bookings || attempts >= number_of_sessions
+      if Booking.create(member, sessions.sample()) == "Done"
+        bookings_made += 1
+      end
+      attempts += 1
     end
   end
 end
